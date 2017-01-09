ec376722a8641f779a0fee80bf34cca71efea872
tracer-ics2015 <tracer@njuics.org>
2017-01-09 22:44:07 +0800

    >  compile NEMU 141220008 cjc Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:44:07 up 30 min,  3 users,  load average: 1.80, 1.62, 1.32 3687c95a7dde6bd0bb2d46a315eaa9013e476d15

---
+++ obj/nemu/cpu/exec/raise_intr.d
@@ -0,0 +1,5 @@
+obj/nemu/cpu/exec/raise_intr.o: nemu/src/cpu/exec/raise_intr.c \
+ nemu/include/cpu/reg.h nemu/include/common.h nemu/include/debug.h \
+ nemu/include/macro.h nemu/include/cpu/../../../lib-common/x86-inc/cpu.h \
+ nemu/include/cpu/../../../lib-common/x86-inc/mmu.h \
+ nemu/include/../../lib-common/x86-inc/mmu.h
---
+++ obj/nemu/memory/L1_cache.d
@@ -0,0 +1,4 @@
+obj/nemu/memory/L1_cache.o: nemu/src/memory/L1_cache.c \
+ nemu/src/memory/cache.h nemu/include/common.h nemu/include/debug.h \
+ nemu/include/macro.h nemu/src/memory/burst.h nemu/include/misc.h \
+ nemu/include/common.h nemu/src/memory/rand.h
